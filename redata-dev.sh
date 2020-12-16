c1a875cf5db90b32b390a345f5bf7313c69d9ad3
Mateusz Klimek <matkk11@gmail.com>
2020-12-16 12:49:36 +0100

    Update to dev setup

--- redata-dev.sh
+++ redata-dev.sh
@@ -1,1 +1,1 @@
-docker exec -it redata_scheduler_airflow_1 python /usr/local/redata/redata-dev/command_line.py $@
+docker exec -it redata_scheduler_airflow_1 python /usr/local/redata-dev/redata/command_line.py $@
