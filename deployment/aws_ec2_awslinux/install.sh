e58baf2945f354544c5db974182a30c7b0cacd13
Mateusz Klimek <matkk11@gmail.com>
2020-12-28 14:00:33 +0100

    scripts for deploying in aws (#15)
    Add instructions for deploying on AWS

---
+++ deployment/aws_ec2_awslinux/install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker $USER
+
+sudo wget https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m) -O /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
+
+mkdir redata && cd redata
+wget https://raw.githubusercontent.com/redata-team/redata/master/{env_template,docker-compose.yml}
+mv env_template .env
