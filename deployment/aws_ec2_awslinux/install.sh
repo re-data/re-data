d72c2feac3f945424a8ffb19e53a425c68ec7f3b
Mateusz Klimek <matkk11@gmail.com>
2020-12-28 13:52:21 +0100

    add update

--- deployment/aws_ec2_awslinux/install.sh
+++ deployment/aws_ec2_awslinux/install.sh
@@ -10,5 +10,5 @@ sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
 
 mkdir redata && cd redata
-wget https://raw.githubusercontent.com/airbytehq/airbyte/master/{env_template,docker-compose.yaml}
-cp env_template .env
+wget https://raw.githubusercontent.com/redata-team/redata/master/{env_template,docker-compose.yml}
+mv env_template .env
