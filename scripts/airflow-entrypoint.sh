2530a1b3ea2fbfaf503726062ea9105c07e2e0c0
Mateusz Klimek <matkk11@gmail.com>
2021-01-08 12:33:42 +0100

    Update to create user from .env file

--- scripts/airflow-entrypoint.sh
+++ scripts/airflow-entrypoint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 airflow db upgrade
-airflow users create -r Admin -u admin -e admin@example.com -f Admin -l User -p admin
+airflow users create -r Admin -u ${AIRFLOW_SECURITY_ADMIN_USER} -e ${AIRFLOW_SECURITY_ADMIN_EMAIL} -f Admin -l User -p ${AIRFLOW_SECURITY_ADMIN_PASSWORD}
 airflow webserver
