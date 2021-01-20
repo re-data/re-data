5aee2902238cdb9b9748102d36b76667fec69ebc
Gabriele Bertoli <59700888+gab-shell@users.noreply.github.com>
2021-01-20 09:32:49 +0100

    Removing some commented code

--- tests/db_fixtures.py
+++ tests/db_fixtures.py
@@ -11,27 +11,6 @@ import pdb
 
 # pdb.set_trace()
 
-# @pytest.fixture(scope='function')
-# def setup_db(postgresql_my):
-
-#     def dbcreator():
-#         '''
-#         We want to use SQLAlchemy for it's object models to create tables. To do so we need the cursor object to pass to the engine. This function returns a connection.
-
-#         Returns
-#         -------
-#         [type]
-#             [description]
-#         '''
-#         return postgresql_my.cursor().connection
-
-#     engine = create_engine('postgresql+psycopg2://', creator=dbcreator)
-#     Base.metadata.create_all(engine)
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-#     yield session
-#     session.close()
-
 # POSTGRES_USER = os.environ['REDATA_METRICS_DATABASE_USER']
 # POSTGRES_PWD = os.environ['REDATA_METRICS_DATABASE_PASSWORD']
 # POSTGRES_HOST = os.environ['REDATA_METRICS_DATABASE_HOST']
