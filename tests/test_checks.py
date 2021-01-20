79ad891866da5b3b2faab77eea846d64ab08008c
Gabriele Bertoli <59700888+gab-shell@users.noreply.github.com>
2021-01-20 09:33:58 +0100

    Populating the test_check_data_delayed function

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -1,25 +1,44 @@
-# STL imports
-
 # Third party imports
 import pytest
 import pdb
 
-# Custom imports
-# from redata.checks.data_delayed import (
-#     check_data_delayed
-# )
+MAKE_Q = "CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);"
+INSERT_Q = "INSERT INTO test VALUES(1, 2, 'c');"
+SELECT_Q = "SELECT * FROM test;"
 
 
-def test_check_data_delayed():
-    pass
+def test_check_data_delayed(postgresql):
+    #pdb.set_trace()
+    from redata.checks.data_delayed import check_data_delayed  # pylint:disable=import-outside-toplevel
 
-
-def test_example_postgres(postgresql):
-    """Check main postgresql fixture."""
+    # Create a table in the database
     cur = postgresql.cursor()
-    cur.execute(
-        "CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
-    postgresql.commit()
+    cur.execute(MAKE_Q)
+
+    # Add a row to the table
+    cur.execute(INSERT_Q)
+
+    # Get the tables
+    cur.execute(SELECT_Q)
+    table = cur.fetchall()
+
+    from redata.db_operations import get_db_object
+
+    db = get_db_object(postgresql)
+
+    check_data_delayed(db, table)
+
     cur.close()
 
-    #pdb.set_trace()
+
+# def test_example_postgres(postgresql):
+#     """Check main postgresql fixture."""
+#     # pdb.set_trace()
+#     # from redata.checks.data_delayed import (
+#     #     check_data_delayed
+#     # )
+#     cur = postgresql.cursor()
+#     cur.execute(
+#         "CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+#     postgresql.commit()
+#     cur.close()
