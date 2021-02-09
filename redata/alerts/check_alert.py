45694d750081ecdec6e31ec0d80d5b9b3472418b
Mateusz Klimek <matkk11@gmail.com>
2021-02-09 18:14:36 +0100

    Run alerts only for daily metrics

--- redata/alerts/check_alert.py
+++ redata/alerts/check_alert.py
@@ -7,15 +7,15 @@ from redata.alerts.base import alert_on_z_score, get_last_results
 from redata import settings
 
 
+
 def volume_alert(db, table, conf):
 
     sql_df = get_last_results(db, table, 'metrics_data_volume', conf)
-    
-    for interval in settings.VOLUME_INTERVAL:
-        filtered = sql_df[sql_df['time_interval'] == interval]    
-        checked_txt = f'volume in interval: {interval}'
 
-        alert_on_z_score(filtered, table, 'count', 'volume_alert', checked_txt, conf)
+    filtered = sql_df[sql_df['time_interval'] == settings.INTERVAL_FOR_ALERTS]    
+    checked_txt = f'volume in interval: {settings.INTERVAL_FOR_ALERTS}'
+
+    alert_on_z_score(filtered, table, 'count', 'volume_alert', checked_txt, conf)
 
 
 def delay_alert(db, table, conf):
@@ -30,19 +30,20 @@ def values_alert(db, table, conf):
 
     sql_df = get_last_results(db, table, 'metrics_data_values', conf)
 
-    checks_df = sql_df[['check_name', 'time_interval', 'column_name', 'column_value']].drop_duplicates()
+    sql_df = sql_df[sql_df['time_interval'] == settings.INTERVAL_FOR_ALERTS]
+
+    checks_df = sql_df[['check_name', 'column_name', 'column_value']].drop_duplicates()
 
     for i, row in checks_df.iterrows():
 
         df = sql_df[
             (sql_df['check_name'] == row['check_name']) &
-            (sql_df['time_interval'] == row['time_interval']) &
             (sql_df['column_name'] == row['column_name'])
         ]
 
         if row['column_value']:
             df = df[df['column_value'] == row['column_value']]
 
-        check_text = f'values for {row.check_name} in column: {row.column_name}, interval: {row.time_interval}'
+        check_text = f'column: {row.column_name}, values for {row.check_name}, interval: {row.time_interval}'
         alert_on_z_score(df, table, 'check_value', row['check_name'], check_text, conf)
     
