9be48daeb06d1867cdd52ab3eabb511fd91d59b2
Guillermo <50501108+guicalare@users.noreply.github.com>
2021-02-10 13:25:54 +0100

    mahalanobis distance indicator (#45)
    Add statistics functions
    
    Co-authored-by: Guillermo-C-A <50501108+Guillermo-C-A@users.noreply.github.com>

---
+++ tests/mahalanobis_distance/mahalanobis_test.py
@@ -0,0 +1,11 @@
+if __name__ == "__main__":
+    
+    from pandas import read_csv
+    from numpy import mean, cov
+    from redata.statistics.mahalonobis_distance import MahalanobisDistance
+
+    data = read_csv("../../sample_sources/data.csv", sep=",")
+
+    data = data.values.T
+
+    MahalanobisDistance().multidimensional_outliers(data, mean(data, axis=1), cov(data))
