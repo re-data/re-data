2abf117854e84202307011e95963f6b6870084a9
Mateusz Klimek <matkk11@gmail.com>
2021-02-15 15:32:55 +0100

    Add models

---
+++ redata/checks/create.py
@@ -0,0 +1,30 @@
+from redata.models.checks import Check
+from redata.db_operations import metrics_db, metrics_session
+
+standard_checks = [
+    {'func': 'data_schema.check_if_schema_changed', 'params': {}},
+    {'func': 'data_delayed.check_data_delayed', 'params': {}},
+    {'func': 'data_volume.check_data_volume_diff', 'params': {}},
+    {'func': 'data_volume.check_data_volume', 'params': {'time_interval': '1 day'}},
+]
+
+def create_for_detected_table(table):
+
+    for check in standard_checks:
+
+        func = check['func']
+        func_name = func.split('.')[-1],
+
+        model_check = Check(
+            table_id=table.id,
+            name=func_name,
+            columns=None,
+            metrics=[func_name],
+            query = {
+                'type': 'standard',
+                'path': f'redata.checks.{func}',
+                'params': check['params']
+            }
+        )
+
+        metrics_session.add(model_check)
---
+++ redata/models/checks.py
@@ -0,0 +1,21 @@
+from sqlalchemy import TIMESTAMP, Boolean, Column, Integer, String, JSON, BigInteger, Date, Float, Index, ARRAY 
+from redata.models.base import Base
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy import ForeignKey
+from datetime import datetime
+
+
+class Check(Base):
+    __tablename__ = 'checks'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    table_id = Column(Integer, ForeignKey('monitored_table.id'), index=True, nullable=False)
+    
+    name = Column(String, nullable=False)
+
+    columns = Column(ARRAY(String))
+    metrics = Column(ARRAY(String))
+
+    query = Column(JSONB, nullable=False)
+
+    created_at = Column(TIMESTAMP, default=datetime.utcnow, index=True)
