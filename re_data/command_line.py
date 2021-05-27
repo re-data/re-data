1925d3d657bff65b24bcf5acc732eec003432bde
Mateusz Klimek <matkk11@gmail.com>
2021-05-27 16:13:23 +0200

    Update names

--- re_data/command_line.py
+++ re_data/command_line.py
@@ -56,9 +56,9 @@ def run(start_date, end_date, full_refresh):
         days_back = (date.today() - for_date.date()).days - 1
 
         dbt_vars = {
-            'redata:time_window_start': str(for_date),
-            'redata:time_window_end': str(for_date + timedelta(days=1)),
-            'redata:anomaly_detection_window_start': str(for_date - timedelta(days=30))
+            're_data:time_window_start': str(for_date),
+            're_data:time_window_end': str(for_date + timedelta(days=1)),
+            're_data:anomaly_detection_window_start': str(for_date - timedelta(days=30))
         }
 
         run_list = ['dbt'] + ['run'] + ['--vars'] + [json.dumps(dbt_vars)]
