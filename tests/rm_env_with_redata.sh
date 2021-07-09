767981aee34111f977f29e53daf3d76df46943aa
Mateusz Klimek <matkk11@gmail.com>
2021-07-09 13:12:14 +0200

    Update testing env

---
+++ tests/rm_env_with_redata.sh
@@ -0,0 +1,2 @@
+rm -rf test_re_data
+pyenv virtualenv-delete --force test_re_data
--- tests/test_re_data_install.sh
+++ tests/test_re_data_install.sh
@@ -3,8 +3,3 @@ echo "test_re_data" >> test_re_data/.python-version
 pyenv virtualenv test_re_data
 cd test_re_data
 pip install re_data
-
-echo "Cleaning testing directories and virtualenvs"
-cd ..
-rm -rf test_re_data
-pyenv virtualenv-delete --force test_re_data
