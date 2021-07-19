79bf17829585e7af82d950502df67ab77cba1c41
Mateusz Klimek <matkk11@gmail.com>
2021-07-19 10:26:51 +0200

    update edit url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -62,13 +62,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/re-data/re-data/edit/master/docs/'
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
