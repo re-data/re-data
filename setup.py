4d0dd7bb81f29e0d445449fd5da579fb6aee5cf3
Mateusz Klimek <matkk11@gmail.com>
2021-07-29 10:34:36 +0200

    Bumb to beta version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="re_data",
-    version="0.2.0a",
+    version="0.2.0b",
     author="redata-team",
     author_email="mateusz@getre.io",
     description="re_data - data quality framework",
