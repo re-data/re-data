a9c10a148037d34867cf44d063a25550373dcd9f
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2021-12-06 02:57:27 +0100

    cast last value to number

--- re_data_ui/src/pages/Alerts.tsx
+++ re_data_ui/src/pages/Alerts.tsx
@@ -56,7 +56,7 @@ const Alerts: React.FC = (): ReactElement => {
                                             </div>
                                         </td>
                                         <td className="px-6 py-4 text-sm whitespace-nowrap">
-                                            {anomaly.last_value.toFixed(2)}
+                                            {Number(anomaly.last_value).toFixed(2)}
                                         </td>
                                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                             <Moment format={dateTimeFormat}>{anomaly.time_window_end}</Moment>
