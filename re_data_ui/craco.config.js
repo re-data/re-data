7b65bdd87e705bdfdb2736d5eca51e002417cda3
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2021-12-07 04:35:08 +0100

    bundle into single html file

--- re_data_ui/craco.config.js
+++ re_data_ui/craco.config.js
@@ -1,4 +1,27 @@
+const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 module.exports = {
+    webpack: {
+        configure: (webpackConfig, {env, paths}) => {
+
+            webpackConfig.plugins.forEach(plugin => {
+                if (plugin instanceof InlineChunkHtmlPlugin) {
+                    plugin.tests = [/.+[.]js/]
+                }
+            })
+
+            const oneOfRuleIdx = webpackConfig.module.rules.findIndex(rule => !!rule.oneOf);
+            webpackConfig.module.rules[oneOfRuleIdx].oneOf.forEach(loader => {
+                if (loader.test && loader.test.test && (loader.test.test("test.module.css") || loader.test.test("test.module.scss"))) {
+                    loader.use.forEach(use => {
+                        if (use.loader && use.loader.includes('mini-css-extract-plugin')) {
+                            use.loader = require.resolve('style-loader');
+                        }
+                    })
+                }
+            })
+            return webpackConfig
+        }
+    },
     style: {
         postcss: {
             plugins: [
