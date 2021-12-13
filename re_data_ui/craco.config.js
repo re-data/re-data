60becef2275fd419e4609ef55468636acddf7035
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2021-12-13 03:24:08 +0100

    finalize lint action

--- re_data_ui/craco.config.js
+++ re_data_ui/craco.config.js
@@ -1,33 +1,34 @@
+/* eslint-disable */
 const InlineChunkHtmlPlugin = require('react-dev-utils/InlineChunkHtmlPlugin');
 module.exports = {
-    webpack: {
-        configure: (webpackConfig, {env, paths}) => {
+  webpack: {
+    configure: (webpackConfig, {env, paths}) => {
 
-            webpackConfig.plugins.forEach(plugin => {
-                if (plugin instanceof InlineChunkHtmlPlugin) {
-                    plugin.tests = [/.+[.]js/]
-                }
-            })
-
-            const oneOfRuleIdx = webpackConfig.module.rules.findIndex(rule => !!rule.oneOf);
-            webpackConfig.module.rules[oneOfRuleIdx].oneOf.forEach(loader => {
-                if (loader.test && loader.test.test && (loader.test.test("test.module.css") || loader.test.test("test.module.scss"))) {
-                    loader.use.forEach(use => {
-                        if (use.loader && use.loader.includes('mini-css-extract-plugin')) {
-                            use.loader = require.resolve('style-loader');
-                        }
-                    })
-                }
-            })
-            return webpackConfig
+      webpackConfig.plugins.forEach(plugin => {
+        if (plugin instanceof InlineChunkHtmlPlugin) {
+          plugin.tests = [/.+[.]js/]
         }
+      })
+
+      const oneOfRuleIdx = webpackConfig.module.rules.findIndex(rule => !!rule.oneOf);
+      webpackConfig.module.rules[oneOfRuleIdx].oneOf.forEach(loader => {
+        if (loader.test && loader.test.test && (loader.test.test("test.module.css") || loader.test.test("test.module.scss"))) {
+          loader.use.forEach(use => {
+            if (use.loader && use.loader.includes('mini-css-extract-plugin')) {
+              use.loader = require.resolve('style-loader');
+            }
+          })
+        }
+      })
+      return webpackConfig
+    }
+  },
+  style: {
+    postcss: {
+      plugins: [
+        require('tailwindcss'),
+        require('autoprefixer'),
+      ],
     },
-    style: {
-        postcss: {
-            plugins: [
-                require('tailwindcss'),
-                require('autoprefixer'),
-            ],
-        },
-    },
+  },
 };
