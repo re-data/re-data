9fb2ac52dad98c5e76f0ab6df02f1b0e1e0fab85
Mateusz Klimek <matkk11@gmail.com>
2021-12-22 12:00:46 +0100

    release version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="re_data",
-    version="0.5.0b",
+    version="0.5.0",
     author="redata-team",
     author_email="mateusz@getre.io",
     description="re_data - data quality framework",
