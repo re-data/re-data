999b7407f159765109c7846ccc899701212e9155
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2021-12-28 14:29:49 +0100

    updated screen to use table component

--- re_data_ui/src/components/TableSchema.tsx
+++ re_data_ui/src/components/TableSchema.tsx
@@ -1,5 +1,6 @@
-import React, { PropsWithChildren, ReactElement } from 'react';
+import React, { useMemo, PropsWithChildren, ReactElement } from 'react';
 import { ITableSchema } from '../contexts/redataOverviewContext';
+import Table, { ColumnsProps } from './Table';
 
 interface TableSchemaProps {
   tableSchemas: ITableSchema[]
@@ -8,6 +9,18 @@ const TableSchema: React.FC<TableSchemaProps> = (
   props: PropsWithChildren<TableSchemaProps>,
 ): ReactElement => {
   const { tableSchemas } = props;
+
+  const columns: ColumnsProps[] = useMemo(() => [
+    {
+      Header: 'Column Name',
+      accessor: 'column_name',
+    },
+    {
+      Header: 'Data Type',
+      accessor: 'data_type',
+    },
+  ],
+  []);
   return (
     <>
       <span className="text-lg text--capitalize">Table Schema</span>
@@ -16,46 +29,7 @@ const TableSchema: React.FC<TableSchemaProps> = (
           <div className="-my-2 sm:-mx-6 lg:-mx-8">
             <div className="py-2 align-middle inline-block w-full max-w-full sm:px-6 lg:px-8">
               <div className="shadow overflow-x-auto border-b border-gray-200 sm:rounded-lg">
-                <table className="max-w-full divide-y divide-gray-200 w-full">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Column Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Data Type
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
-
-                    {tableSchemas.map((tableSchema) => (
-                      <tr key={tableSchema.column_name}>
-                        <td className="px-6 text-sm py-4 whitespace-nowrap">
-                          <div
-                            className="text-gray-900"
-                          >
-                            {tableSchema.column_name}
-                          </div>
-                        </td>
-                        <td className="px-6 text-sm py-4 whitespace-nowrap">
-                          <div
-                            className="text-gray-900"
-                          >
-                            {tableSchema.data_type}
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-
-                  </tbody>
-                </table>
+                <Table columns={columns} data={tableSchemas} showSearch={false} />
               </div>
             </div>
           </div>
