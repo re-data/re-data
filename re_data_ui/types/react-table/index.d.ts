a9b52ce4398959206fcdbaac3dd0745b8f828d6d
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2021-12-29 21:08:11 +0100

    clean up

--- re_data_ui/types/react-table/index.d.ts
+++ re_data_ui/types/react-table/index.d.ts
@@ -60,9 +60,6 @@ declare module 'react-table' {
         UseRowSelectOptions<D>,
         UseRowStateOptions<D>,
         UseSortByOptions<D>,
-        // note that having Record here allows you to add anything to the options, this matches the spirit of the
-        // underlying js library, but might be cleaner if it's replaced by a more specific type that matches your
-        // feature set, this is a safe default.
         Record<string, any> {}
 
     export interface Hooks<D extends Record<string, unknown> = Record<string, unknown>>
