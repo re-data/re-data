42009c77d07512c0ada988e5202debf092a80cb5
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-01-10 15:07:12 +0100

    relative links for tables screens

--- re_data_ui/src/pages/Tables.tsx
+++ re_data_ui/src/pages/Tables.tsx
@@ -1,12 +1,15 @@
+/* eslint-disable comma-dangle */
 import React, {
-  ReactElement, useContext, useMemo, useState,
+  ReactElement, useContext, useMemo, useState
 } from 'react';
 import { BiHappyAlt } from 'react-icons/all';
 import { useSearchParams } from 'react-router-dom';
 import {
-  TableSchema, SchemaChanges, Select, EmptyContent, MetricCharts,
+  EmptyContent, MetricCharts, SchemaChanges, Select, TableSchema
 } from '../components';
-import { OverviewData, ReDataModelDetails, RedataOverviewContext } from '../contexts/redataOverviewContext';
+import {
+  OverviewData, ReDataModelDetails, RedataOverviewContext
+} from '../contexts/redataOverviewContext';
 import useModel from '../hooks/useModel';
 
 type optionsProps = {
@@ -29,6 +32,7 @@ const generateOptions = (models: Map<string, ReDataModelDetails>) => {
 const Tables: React.FC = (): ReactElement => {
   const overview: OverviewData = useContext(RedataOverviewContext);
   const { aggregated_models: models } = overview;
+  const [activeTab, setActiveTab] = useState('');
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [_, setURLSearchParams] = useSearchParams();
 
@@ -41,14 +45,19 @@ const Tables: React.FC = (): ReactElement => {
     if (option) {
       const details = init(overview, option?.value);
       setModelDetails(details);
-      setURLSearchParams({ model: option.value });
+      // setURLSearchParams({ model: option.value });
     }
   };
 
   const options = useMemo(() => generateOptions(models), [models]) || [];
 
+  const handleScroll = (idName: string) => {
+    setActiveTab(idName);
+    document?.getElementById(idName)?.scrollIntoView();
+  };
+
   return (
-    <div className="grid grid-cols-1 overflow-y-scroll">
+    <div className="grid grid-cols-1">
       <h1 className="mb-3 text-2xl font-semibold">Tables</h1>
       <div className="w-1/3 ml-1 mb-4">
         <Select
@@ -60,39 +69,79 @@ const Tables: React.FC = (): ReactElement => {
 
       {modelDetails ? (
         <div className="flex flex-col w-full">
-          <section className="bg-white rounded-md px-3 py-4 mb-4">
-            <h3 className="mb-3 text-md font-medium">Anomalies</h3>
-            <div className="outlet">
-              <MetricCharts
-                modelDetails={modelDetails}
-                showAnomalies
-                showTitle={false}
-                fullWidth={false}
-              />
+          <nav className="top-0 sticky bg-gray-100 py-1 mb-1">
+            <ul className="flex align-items">
+              <li
+                className={`mr-4 ${activeTab === 'anomalies' && 'active-tab'}`}
+              >
+                <a
+                  href="./#/tables/#anomalies"
+                  onClick={() => handleScroll('anomalies')}
+                >
+                  Anomalies
+                </a>
+              </li>
+              <li
+                className={`mr-4 ${activeTab === 'metrics' && 'active-tab'}`}
+              >
+                <a
+                  href="./#/tables/#metrics"
+                  onClick={() => handleScroll('metrics')}
+                >
+                  Metrics
+                </a>
+              </li>
+              <li
+                className={`mr-4 ${activeTab === 'schema' && 'active-tab'}`}
+              >
+                <a
+                  href="./#/tables/#schema"
+                  onClick={() => handleScroll('schema')}
+                >
+                  Schema
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <section id="anomalies" className="pb-4 pt-16 -mt-12">
+            <div className="bg-white rounded-md px-3 py-4">
+              <h3 className="mb-3 text-md font-medium">Anomalies</h3>
+              <div className="outlet">
+                <MetricCharts
+                  modelDetails={modelDetails}
+                  showAnomalies
+                  showTitle={false}
+                  fullWidth={false}
+                />
+              </div>
             </div>
           </section>
-          <section className="bg-white rounded-md px-3 py-4 mb-4">
-            <h3 className="mb-3 text-md font-medium">Metics</h3>
-            <div className="outlet">
-              <MetricCharts
-                modelDetails={modelDetails}
-                showAnomalies={false}
-                showTitle={false}
-                fullWidth={false}
-              />
+          <section id="metrics" className="pb-4 pt-16 -mt-16">
+            <div className="bg-white rounded-md px-3 py-4">
+              <h3 className="mb-3 text-md font-medium">Metics</h3>
+              <div className="outlet">
+                <MetricCharts
+                  modelDetails={modelDetails}
+                  showAnomalies={false}
+                  showTitle={false}
+                  fullWidth={false}
+                />
+              </div>
             </div>
           </section>
-          <section className="bg-white rounded-md px-3 py-4 mb-4">
-            <h3 className="mb-3 text-md font-medium">Schema</h3>
-            <div className="grid grid-cols-2 gap-4">
-              <TableSchema
-                tableSchemas={modelDetails.tableSchema}
-                showTitle={false}
-              />
-              <SchemaChanges
-                modelDetails={modelDetails}
-                showTitle={false}
-              />
+          <section id="schema" className="pb-4 pt-4">
+            <div className="bg-white rounded-md px-3 py-4">
+              <h3 className="mb-3 text-md font-medium">Schema</h3>
+              <div className="grid grid-cols-2 gap-4">
+                <TableSchema
+                  tableSchemas={modelDetails.tableSchema}
+                  showTitle={false}
+                />
+                <SchemaChanges
+                  modelDetails={modelDetails}
+                  showTitle={false}
+                />
+              </div>
             </div>
           </section>
         </div>
