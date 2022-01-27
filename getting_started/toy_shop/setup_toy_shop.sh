8bd6e74149f9b3297753ddfe07a786c27bdd898e
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2022-01-27 08:51:37 +0100

    update tests to run for a shorter period

--- getting_started/toy_shop/setup_toy_shop.sh
+++ getting_started/toy_shop/setup_toy_shop.sh
@@ -6,11 +6,11 @@ dbt run-operation create_toy_shop_source_tables --profile $dbt_profile
 dbt seed --profile $dbt_profile
 dbt run --exclude package:re_data --profile $dbt_profile
 dbt test --exclude package:re_data --profile $dbt_profile || true
-re_data run --start-date 2021-01-01 --end-date 2021-01-15 --profile $dbt_profile
+re_data run --start-date 2021-01-01 --end-date 2021-01-06 --profile $dbt_profile
 dbt test --exclude package:re_data --profile $dbt_profile || true
 dbt run-operation schema_change_add_orders_column --profile $dbt_profile
-re_data run --start-date 2021-01-15 --end-date 2021-01-16 --profile $dbt_profile
+re_data run --start-date 2021-01-06 --end-date 2021-01-07 --profile $dbt_profile
 dbt run-operation schema_change_drop_orders_column --profile $dbt_profile
-re_data run --start-date 2021-01-16 --end-date 2021-01-31 --profile $dbt_profile
+re_data run --start-date 2021-01-07 --end-date 2021-01-10 --profile $dbt_profile
 dbt test --exclude package:re_data --profile $dbt_profile || true
 re_data overview generate --start-date 2021-01-01 --end-date 2022-06-30 --profile $dbt_profile
