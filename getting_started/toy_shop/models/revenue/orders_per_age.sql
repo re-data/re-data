60cf43231ac8ffb527066b493916cf4386151d08
Mateusz Klimek <mateusz@getre.io>
2022-01-31 22:53:29 +0100

    update

--- getting_started/toy_shop/models/revenue/orders_per_age.sql
+++ getting_started/toy_shop/models/revenue/orders_per_age.sql
@@ -1,3 +1,7 @@
+{{
+    config(re_data_metrics={'table': ['orders_above_100']})
+}}
+
 select o.id, o.amount, o.status, c.age, o.created_at from 
     {{ ref('orders') }} o, {{ source(target.schema + '_sources', 'toy_shop_customers') }} c
 where
