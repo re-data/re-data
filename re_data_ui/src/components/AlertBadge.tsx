d9531380c74394074400d443b14213aaa230ad08
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-03-10 12:53:55 +0100

    ui fixes

--- re_data_ui/src/components/AlertBadge.tsx
+++ re_data_ui/src/components/AlertBadge.tsx
@@ -8,18 +8,18 @@ const AlertBadge: React.FC<AlertBadgeProps> = (
   props: PropsWithChildren<AlertBadgeProps>,
 ): ReactElement => {
   const { error } = props;
-  let classNames = 'badge mb-3 rounded-full px-2 pt-0.5 pb-1.5 text-center object-right-top text-white text-xs mr-3';
+  let classNames = 'badge rounded-full px-2 pt-1 pb-1.5 text-center object-right-top text-white text-xs mr-3';
   if (error) {
     classNames += ' bg-red-500';
   } else {
     classNames += ' bg-yellow-300';
   }
   return (
-    <span
+    <div
       className={classNames}
     >
       {error ? 'anomaly' : 'schema change'}
-    </span>
+    </div>
   );
 };
 
