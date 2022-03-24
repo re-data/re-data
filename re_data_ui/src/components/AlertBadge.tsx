38e60d8c4f39a8fc24043cf302b90f1ee972dce2
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-03-24 12:20:03 +0100

    handled anomaly in alert baddge

--- re_data_ui/src/components/AlertBadge.tsx
+++ re_data_ui/src/components/AlertBadge.tsx
@@ -9,17 +9,16 @@ const AlertBadge: React.FC<AlertBadgeProps> = (
 ): ReactElement => {
   const { label } = props;
   let classNames = 'badge rounded-full px-2.5 pt-1 pb-1.5 text-center object-right-top text-white text-xs mr-3';
-  if (label === 'anomalies') {
+  if (label === 'anomalies' || label === 'anomaly') {
     classNames += ' bg-secondary';
   } else if (label === 'schema_changes') {
     classNames += ' bg-yellow-300';
   } else if (label === 'test_failure') {
-    // TODO: change this
     classNames += ' bg-red-500';
   }
   return (
     <span className={classNames}>
-      {label?.split('_')?.join(' ')}
+      {label?.split('_')?.join(' ')?.toString()}
     </span>
   );
 };
