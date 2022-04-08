cbc4813eac5409a9ae29aed61e546683e8552007
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-04-08 11:57:36 +0100

    copy to clipboard text

--- re_data_ui/src/partials/MetaData.tsx
+++ re_data_ui/src/partials/MetaData.tsx
@@ -1,6 +1,5 @@
-import React, { useState } from 'react';
+import React, { Fragment, useState } from 'react';
 import { format } from 'sql-formatter';
-import { FaRegClipboard } from 'react-icons/all';
 import CodeFormatter from './CodeFormatter';
 
 type MetaDataType = {
@@ -20,64 +19,63 @@ const MetaData = ({ failuresJson, compiledSql }: MetaDataType): JSX.Element => {
     <section>
       {(failuresJson || compiledSql) && (
         <>
-          <ul className="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 mt-4">
-            {failuresJson && (
-            <li className="mr-2">
+          <ul className="flex justify-between flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 mt-4">
+            <Fragment key="left">
+              {failuresJson && (
+              <li className="mr-2">
+                <button
+                  onClick={() => setActiveTab('failures_json')}
+                  type="button"
+                  className={`inline-block pl-0 p-4 rounded-t-lg ${activeTab === 'failures_json' ? 'text-primary font-semibold' : ''}`}
+                >
+                  Failure Json
+                </button>
+              </li>
+              )}
+
+              {compiledSql && (
+              <li className="mr-2">
+                <button
+                  onClick={() => setActiveTab('compiled_sql')}
+                  type="button"
+                  className={`inline-block pl-0 p-4 rounded-t-lg ${activeTab === 'compiled_sql' ? 'text-primary font-semibold' : ''}`}
+                >
+                  Compiled SQL
+                </button>
+              </li>
+              )}
+            </Fragment>
+
+            <li className="flex-end">
               <button
-                onClick={() => setActiveTab('failures_json')}
+                onClick={() => copyToClipboard(
+                  activeTab === 'failures_json' ? failuresJsonCode : compiledSqlCode,
+                )}
                 type="button"
-                className={`inline-block pl-0 p-4 rounded-t-lg ${activeTab === 'failures_json' ? 'text-primary font-semibold' : ''}`}
+                className="inline-block p-4 rounded-t-lg text-black copy-icon font-semibold"
               >
-                Failure Json
+                Copy to clipboard
               </button>
             </li>
-            )}
-
-            {compiledSql && (
-            <li className="mr-2">
-              <button
-                onClick={() => setActiveTab('compiled_sql')}
-                type="button"
-                className={`inline-block pl-0 p-4 rounded-t-lg ${activeTab === 'compiled_sql' ? 'text-primary font-semibold' : ''}`}
-              >
-                Compiled SQL
-              </button>
-            </li>
-            )}
-
           </ul>
+
           {activeTab === 'failures_json' && failuresJson && (
-          <div className="mt-5">
-            <h6 className="font-semibold">Failures Json</h6>
+          <div className="mt-3">
             <div className="flex flex-col mt-2 rounded-md overflow-hidden">
               <CodeFormatter
                 code={failuresJsonCode}
                 language="json"
-              />
-
-              <FaRegClipboard
-                size={16}
-                color="white"
-                onClick={() => copyToClipboard(failuresJsonCode)}
-                className="absolute right-2 bottom-2 cursor-pointer copy-icon"
               />
             </div>
           </div>
           )}
 
           {activeTab === 'compiled_sql' && compiledSql && (
-          <div className="mt-5">
-            <h6 className="font-semibold">Compiled SQL</h6>
-            <div className="flex flex-col mt-2 rounded-md overflow-hidden relative">
+          <div className="mt-3">
+            <div className="flex flex-col rounded-md overflow-hidden relative">
               <CodeFormatter
                 code={compiledSqlCode}
                 language="sql"
-              />
-              <FaRegClipboard
-                size={16}
-                color="white"
-                onClick={() => copyToClipboard(compiledSqlCode)}
-                className="absolute right-2 bottom-2 cursor-pointer copy-icon"
               />
             </div>
           </div>
