5aee3c93f80b6b68638b690e4abe09086c3175a8
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-05-24 11:45:34 +0100

    in dev

--- re_data_ui/src/pages/Dashboard.tsx
+++ re_data_ui/src/pages/Dashboard.tsx
@@ -19,6 +19,7 @@ import {
   SelectOptionProps,
   TableSample,
   TestData,
+  MonitoredData,
 } from '../contexts/redataOverviewContext';
 import {
   appendToMapKey,
@@ -29,6 +30,7 @@ import {
   PROJECT_NAME,
   RE_DATA_OVERVIEW_FILE,
   stripQuotes,
+  MONITORED_FILE,
   supportedResTypes,
   TABLE_SAMPLE_FILE,
   TEST_FILE,
@@ -302,6 +304,7 @@ const Dashboard: React.FC = (): ReactElement => {
     modelTestMapping: {},
     testNameMapping: {},
     tableSamples: new Map<string, TableSample>(),
+    monitoredData: [],
   };
   const [reDataOverview, setReDataOverview] = useState<OverviewData>(initialOverview);
   const prepareOverviewData = async (): Promise<void> => {
@@ -315,22 +318,52 @@ const Dashboard: React.FC = (): ReactElement => {
         dbtManifestRequest,
         testRequest,
         metadataRequest,
+        monitoredRequest,
         tableSamplesRequest,
       ] = await Promise.all([
         fetch(RE_DATA_OVERVIEW_FILE, { headers }),
         fetch(DBT_MANIFEST_FILE, { headers }),
         fetch(TEST_FILE, { headers }),
         fetch(METADATA_FILE, { headers }),
+        fetch(MONITORED_FILE, { headers }),
         fetch(TABLE_SAMPLE_FILE, { headers }),
       ]);
       const overviewData: Array<RawOverviewData> = await overviewRequest.json();
       const graphData: DbtGraph = await dbtManifestRequest.json();
       const metaData: MetaData = await metadataRequest.json();
       const testData: Array<TestData> = await testRequest.json();
+      const monitoredRes = await monitoredRequest.json();
       const tableSamples: Array<TableSample> = await tableSamplesRequest.json();
 
       const tableSamplesData = new Map<string, TableSample>();
+      const monitoredData = [];
 
+      // console.log('monitoredData ', monitoredRes);
+
+      for (let index = 0; index < monitoredRes.length; index++) {
+        let {
+          anomaly_detector, columns, metrics, model, owners,
+        } = monitoredRes[index];
+
+        model = model.replaceAll('"', '');
+        anomaly_detector = JSON.parse(anomaly_detector);
+        columns = JSON.parse(columns);
+        metrics = JSON.parse(metrics);
+        owners = JSON.parse(owners);
+
+        // console.log('res ', (anomaly_detector), (columns), (metrics), (model), (owners));
+
+        monitoredData.push({
+          anomalyDetector: anomaly_detector,
+          columns,
+          metrics,
+          model,
+          owners,
+          timeFilter: monitoredRes[index].time_filter,
+        });
+      }
+
+      // console.log('monitoredData ', monitoredData);
       for (let index = 0; index < tableSamples.length; index++) {
         let { table_name, sampled_on, sample_data } = tableSamples[index];
         table_name = table_name.replaceAll('"', '');
@@ -364,6 +397,7 @@ const Dashboard: React.FC = (): ReactElement => {
         macroModelUsedIn: {},
         macroDepends: {},
         tableSamples: new Map<string, TableSample>(),
+        monitoredData: [],
       };
       const {
         dbtMapping,
@@ -414,6 +448,7 @@ const Dashboard: React.FC = (): ReactElement => {
       overview.runAts = runAts;
       overview.modelTestMapping = modelTestMapping;
       overview.tableSamples = tableSamplesData;
+      overview.monitoredData = monitoredData;
 
       console.log('overview -> ', overview);
       setReDataOverview(overview);
--- re_data_ui/src/pages/Settings.tsx
+++ re_data_ui/src/pages/Settings.tsx
@@ -1,7 +1,124 @@
-import React, { FC, ReactElement } from 'react';
+import React, {
+  FC,
+  ReactElement,
+  useCallback,
+  useContext,
+  useState,
+} from 'react';
+import {
+  BsFillArrowDownCircleFill,
+  BsFillArrowUpCircleFill,
+} from 'react-icons/all';
+import {
+  OverviewData,
+  RedataOverviewContext,
+} from '../contexts/redataOverviewContext';
 
 const Settings: FC = (): ReactElement => {
-  console.log('settings screen');
+  const [toggleAccordion, setToggleAccordion] = useState<number | null>(0);
+
+  const overview: OverviewData = useContext(RedataOverviewContext);
+  const { monitoredData } = overview;
+  // console.log('settings screen ', monitoredData, loading);
+
+  const toggleThisAccordion = useCallback((index: number) => {
+    if (toggleAccordion === index) {
+      setToggleAccordion(null);
+    } else {
+      setToggleAccordion(index);
+    }
+  }, []);
+
+  const renderReDataInformation = () => {
+    const result = [];
+
+    for (let index = 0; index < monitoredData.length; index++) {
+      const {
+        anomalyDetector, columns, metrics, model, owners, timeFilter,
+      } = monitoredData[index];
+
+      console.log('element ', index, monitoredData[index]);
+
+      result.push(
+        <div
+          className={`py-5 ${
+            monitoredData.length !== index + 1 ? 'border-b-2' : 'border-b-0'
+          }`}
+          key={index}
+        >
+          <div className="flex justify-between bg-white accordion-head">
+            <h3 className="font-bold">{model}</h3>
+            <button
+              type="button"
+              className="cursor-pointer"
+              onClick={() => toggleThisAccordion(index)}
+            >
+              {toggleAccordion === index ? (
+                <BsFillArrowUpCircleFill size="1.25em" />
+              ) : (
+                <BsFillArrowDownCircleFill size="1.25em" />
+              )}
+            </button>
+          </div>
+          <div
+            className={`pr-10 pt-5 accordion-content text-sm ${
+              toggleAccordion === index ? 'active' : ''
+            }`}
+          >
+            <p className="mb-4">
+              <span className="font-semibold">Anomaly detectors: </span>
+              <ul className="list-disc ml-4  mt-2">
+                {Object.keys(anomalyDetector).map((key) => (
+                  <li key={key}>
+                    {key}
+                    {anomalyDetector[key]
+                      ? ` (${anomalyDetector[key]})`
+                      : ''}
+                  </li>
+                ))}
+              </ul>
+            </p>
+            <p className="mb-4">
+              <span className="font-semibold">Metrics: </span>
+              <ul className="list-disc ml-4  mt-2">
+                {Object.keys(metrics).map((key) => (
+                  <li key={key}>
+                    {key}
+                    {metrics[key]
+                      ? ` (${metrics[key]})`
+                      : ''}
+                  </li>
+                ))}
+              </ul>
+            </p>
+            <p className="mb-4">
+              <span className="font-semibold">Owners: </span>
+              <ul className="list-disc ml-4  mt-2">
+                {Object.keys(owners).map((key) => (
+                  <li key={key}>
+                    {key}
+                    {owners[key]
+                      ? ` (${owners[key]})`
+                      : ''}
+                  </li>
+                ))}
+              </ul>
+            </p>
+            <p className="mb-4">
+              <span className="font-semibold">Columns: </span>
+              {columns?.join(', ')}
+            </p>
+            <p className="mb-4">
+              <span className="font-semibold">Time filter: </span>
+              {timeFilter}
+            </p>
+          </div>
+        </div>,
+      );
+    }
+    return result;
+  };
+
   return (
     <section className="border border-red-500 h-full w-full">
       <h2 className="mb-8 text-2xl font-bold">Settings</h2>
@@ -9,7 +126,7 @@ const Settings: FC = (): ReactElement => {
         <aside className="border border-red-500 p-4 pl-0 rounded-md bg-white shadow-lg">
           <ul>
             <li className="text-sm font-medium p-1 pl-0 mb-2">
-              <a href="./id">Re_data</a>
+              <a href="./id">Monitored</a>
             </li>
             <li className="text-sm font-medium p-1 pl-0 mb-2">
               <a href="./id">DBT</a>
@@ -17,7 +134,9 @@ const Settings: FC = (): ReactElement => {
           </ul>
         </aside>
         <div className="col-span-5 border border-red-500 bg-white rounded-md p-4 shadow-lg">
-          <p>Settings screen</p>
+          <div className="accordion border-2 border-[#F3F3F7] mt-5 px-5 mb-20">
+            {renderReDataInformation()}
+          </div>
         </div>
       </div>
     </section>
