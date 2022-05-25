f287486814812701396c1010841617675942c3da
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-05-25 08:37:55 +0100

    completed settings initial screen

--- re_data_ui/src/pages/Settings.tsx
+++ re_data_ui/src/pages/Settings.tsx
@@ -5,10 +5,7 @@ import React, {
   useContext,
   useState,
 } from 'react';
-import {
-  BsFillArrowDownCircleFill,
-  BsFillArrowUpCircleFill,
-} from 'react-icons/all';
+import { BsArrowDownCircle, BsArrowUpCircle } from 'react-icons/all';
 import {
   OverviewData,
   RedataOverviewContext,
@@ -19,15 +16,13 @@ const Settings: FC = (): ReactElement => {
 
   const overview: OverviewData = useContext(RedataOverviewContext);
   const { monitoredData } = overview;
-  // console.log('settings screen ', monitoredData, loading);
 
-  const toggleThisAccordion = useCallback((index: number) => {
-    if (toggleAccordion === index) {
-      setToggleAccordion(null);
-    } else {
-      setToggleAccordion(index);
-    }
-  }, []);
+  const toggleThisAccordion = useCallback(
+    (index: number) => {
+      setToggleAccordion(toggleAccordion === index ? null : index);
+    },
+    [toggleAccordion],
+  );
 
   const renderReDataInformation = () => {
     const result = [];
@@ -37,26 +32,24 @@ const Settings: FC = (): ReactElement => {
         anomalyDetector, columns, metrics, model, owners, timeFilter,
       } = monitoredData[index];
 
-      console.log('element ', index, monitoredData[index]);
-
       result.push(
         <div
-          className={`py-5 ${
-            monitoredData.length !== index + 1 ? 'border-b-2' : 'border-b-0'
+          className={`py-4 ${
+            monitoredData.length !== index + 1 ? 'border-b' : 'border-b-0'
           }`}
           key={index}
         >
           <div className="flex justify-between bg-white accordion-head">
-            <h3 className="font-bold">{model}</h3>
+            <h3 className="font-bold text-lg">{model}</h3>
             <button
               type="button"
               className="cursor-pointer"
               onClick={() => toggleThisAccordion(index)}
             >
               {toggleAccordion === index ? (
-                <BsFillArrowUpCircleFill size="1.25em" />
+                <BsArrowUpCircle size="1.25em" />
               ) : (
-                <BsFillArrowDownCircleFill size="1.25em" />
+                <BsArrowDownCircle size="1.25em" />
               )}
             </button>
           </div>
@@ -65,53 +58,54 @@ const Settings: FC = (): ReactElement => {
               toggleAccordion === index ? 'active' : ''
             }`}
           >
-            <p className="mb-4">
-              <span className="font-semibold">Anomaly detectors: </span>
-              <ul className="list-disc ml-4  mt-2">
+            <div className="mb-4">
+              <p className="font-semibold">Anomaly detectors: </p>
+              <ul className="list-disc ml-4 mt-2">
                 {Object.keys(anomalyDetector).map((key) => (
                   <li key={key}>
                     {key}
-                    {anomalyDetector[key]
-                      ? ` (${anomalyDetector[key]})`
-                      : ''}
+                    {anomalyDetector[key] ? ` (${anomalyDetector[key]})` : ''}
                   </li>
                 ))}
               </ul>
-            </p>
-            <p className="mb-4">
-              <span className="font-semibold">Metrics: </span>
-              <ul className="list-disc ml-4  mt-2">
+            </div>
+            <div className="mb-4">
+              <p className="font-semibold">Metrics: </p>
+              <ul className="list-disc ml-4 mt-2">
                 {Object.keys(metrics).map((key) => (
                   <li key={key}>
                     {key}
-                    {metrics[key]
-                      ? ` (${metrics[key]})`
-                      : ''}
+                    {metrics[key] ? ` (${metrics[key]})` : ''}
                   </li>
                 ))}
               </ul>
-            </p>
-            <p className="mb-4">
-              <span className="font-semibold">Owners: </span>
-              <ul className="list-disc ml-4  mt-2">
-                {Object.keys(owners).map((key) => (
-                  <li key={key}>
-                    {key}
-                    {owners[key]
-                      ? ` (${owners[key]})`
-                      : ''}
-                  </li>
-                ))}
+            </div>
+            <div className="mb-4">
+              <p className="font-semibold">Owners: </p>
+              <ul className="list-disc ml-4 mt-2">
+                {Object.keys(owners).map((key) => {
+                  const { notify_channel: notifyChannel, owner, name } = owners?.[key];
+                  return (
+                    <li key={key} className="mb-4">
+                      {name}
+                      <ul className="list-square ml-4 mt-2">
+                        <li>{`channel: ${notifyChannel}`}</li>
+                        <li>{`channel id: ${key}`}</li>
+                        <li>{`position: ${owner}`}</li>
+                      </ul>
+                    </li>
+                  );
+                })}
               </ul>
-            </p>
-            <p className="mb-4">
-              <span className="font-semibold">Columns: </span>
+            </div>
+            <div className="mb-4">
+              <p className="font-semibold">Columns: </p>
               {columns?.join(', ')}
-            </p>
-            <p className="mb-4">
-              <span className="font-semibold">Time filter: </span>
+            </div>
+            <div className="mb-4">
+              <p className="font-semibold">Time filter: </p>
               {timeFilter}
-            </p>
+            </div>
           </div>
         </div>,
       );
@@ -120,21 +114,21 @@ const Settings: FC = (): ReactElement => {
   };
 
   return (
-    <section className="border border-red-500 h-full w-full">
+    <section className="h-min-full w-full">
       <h2 className="mb-8 text-2xl font-bold">Settings</h2>
-      <div className="grid grid-cols-6 gap-4 border border-red-500">
-        <aside className="border border-red-500 p-4 pl-0 rounded-md bg-white shadow-lg">
-          <ul>
-            <li className="text-sm font-medium p-1 pl-0 mb-2">
-              <a href="./id">Monitored</a>
+      <div className="grid grid-cols-6 gap-4 ">
+        <aside className="rounded-md bg-white shadow-lg">
+          <ul className="my-4">
+            <li className="text-sm font-medium p-3 mb-5 bg-gray-300">
+              Monitored
             </li>
-            <li className="text-sm font-medium p-1 pl-0 mb-2">
-              <a href="./id">DBT</a>
+            <li className="text-sm font-medium p-3 mb-5">
+              DBT
             </li>
           </ul>
         </aside>
-        <div className="col-span-5 border border-red-500 bg-white rounded-md p-4 shadow-lg">
-          <div className="accordion border-2 border-[#F3F3F7] mt-5 px-5 mb-20">
+        <div className="col-span-5  bg-white rounded-md p-4 shadow-lg">
+          <div className="accordion px-2 mb-20">
             {renderReDataInformation()}
           </div>
         </div>
