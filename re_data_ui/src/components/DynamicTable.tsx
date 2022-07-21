4a551f474894a464c71f104b79e9c7192974707b
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-07-21 11:16:41 +0100

    clean up and removed empty table implementation

--- re_data_ui/src/components/DynamicTable.tsx
+++ re_data_ui/src/components/DynamicTable.tsx
@@ -1,12 +1,28 @@
 import React, { useMemo } from 'react';
 import Table, { ColumnsProps } from './Table';
+// import { EmptyTable } from '../partials';
 
 interface DynamicTableType {
   values: Record<string, string>[] | null;
 }
 
 const DynamicTable = ({ values }: DynamicTableType): JSX.Element => {
-  if (!values) return <Table columns={[]} data={[]} showSearch={false} />;
+  console.log('values => ', values);
+  if (!values) {
+    return (
+      <Table
+        columns={[]}
+        data={[]}
+        showSearch={false}
+        // EmptyData={(
+        //   <EmptyTable
+        //     title="Nothing to see here"
+        //     subtitle="You currently do not have any uploads."
+        //   />
+        // )}
+      />
+    );
+  }
   const val = JSON.parse(values as unknown as string) as Record<string, string>[];
 
   const columns: ColumnsProps[] = useMemo(() => {
