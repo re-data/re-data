46eb498b66555bb2c51dd48e7f3b1051f53815c9
Akinyemi Olumide Emmanuel <akinolu52@gmail.com>
2022-08-05 17:00:18 +0100

    show arrow on select

--- re_data_ui/src/partials/TableSelect.tsx
+++ re_data_ui/src/partials/TableSelect.tsx
@@ -17,7 +17,7 @@ function TableSelect({
 }: TableSelectProps): JSX.Element {
   return (
     <select
-      className="form-select appearance-none block w-1/4 px-2 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:shadow-none focus:primary focus:outline-none"
+      className="form-select block w-1/4 px-2 py-1 text-base font-normal text-gray-700 bg-white bg-clip-padding bg-no-repeat border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:shadow-none focus:primary focus:outline-none"
       aria-label="Upload options"
       onChange={handleChange}
       value={value}
