656349f4bbbc2a0c31954414580efeaf24debe70
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2022-11-02 14:05:56 +0100

    revamp email alerting flow

--- re_data/notifications/utils.py
+++ re_data/notifications/utils.py
@@ -56,3 +56,40 @@ def validate_alert_types(selected_alert_types: List[str]):
     for alert_type in selected_alert_types:
         if alert_type not in ALERT_TYPES:
             raise BadOptionUsage("select", "%s not a valid alert type" % alert_type)
+
+
+def create_owners_to_models_map(monitored_models: List) -> Dict[str, List[str]]:
+    model_with_no_owner_key = 'NO_OWNER'
+    owner_mapping = defaultdict(list) 
+    for monitored_model in monitored_models:
+        model = monitored_model['model'].replace('"', '').replace('`', '')
+        model_owners = json.loads(monitored_model.get('owners')) or {}
+
+        has_email_owner = False
+        for email, notify_details in model_owners.items():
+            if notify_details.get('notify_channel') == 'email':
+                has_email_owner = True
+                owner_mapping[email].append(model)
+        # if model has no owner with an email notification channel, treat the model as having no owner
+        if not has_email_owner:
+            owner_mapping[model_with_no_owner_key].append(model)
+
+    return owner_mapping
+
+def create_models_to_alerts_map(alerts: List, selected_alert_types) -> Dict[str, List[str]]:
+    alerts_per_model = {}
+    for alert in alerts:
+        model = alert['model'].replace('"', '').replace('`', '') if alert['model'] else 'No specific model'
+        if model not in alerts_per_model:
+            alerts_per_model[model] = {
+                'anomalies': [],
+                'schema_changes': [],
+                'tests': [],
+            }
+        if alert['type'] == 'anomaly' and alert['type'] in selected_alert_types:
+            alerts_per_model[model]['anomalies'].append(alert)
+        elif alert['type'] == 'schema_change' and alert['type'] in selected_alert_types:
+            alerts_per_model[model]['schema_changes'].append(alert)
+        elif alert['type'] == 'test' and alert['type'] in selected_alert_types:
+            alerts_per_model[model]['tests'].append(alert)
+    return alerts_per_model
